7fd06c929875d97fe7448d12ef67c335b17bbc37
Zzzhenda <victor.victorguti@gmail.com>
2025-11-03 19:47:59 -0300

    catalogo

--- my-new-project/src/main.tsx
+++ my-new-project/src/main.tsx
@@ -3,9 +3,11 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './app'; // (Este será nuestro próximo archivo a crear)
 
+
+
 // 1. Importa el CSS de Bootstrap
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 // 2. Importa tu CSS personalizado
 import './style.css'; 
 
